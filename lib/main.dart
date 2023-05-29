5c89e2c250d797b9952f6e3247df562d4a7dbb33
Chimaobi Onuoha <davidkings1604@gmail.com>
2023-05-29 00:20:34 -0400

    step-3

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:noteme/views/login_view.dart';
+import 'package:noteme/views/notes_view.dart';
 import 'package:noteme/views/register_view.dart';
 import 'package:noteme/views/verify_email_view.dart';
 
@@ -46,7 +47,7 @@ class HomePage extends StatelessWidget {
             } else {
               return const LoginView();
             }
-            return const Text("Done");
+            return const NotesView();
 
           default:
             return const CircularProgressIndicator();
