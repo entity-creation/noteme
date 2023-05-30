bcae99ea78cf2fff92e44e849915d1d7b659d854
Chimaobi Onuoha <davidkings1604@gmail.com>
2023-05-30 00:06:13 -0400

    step_5

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
 
+import 'package:noteme/constants/routes.dart';
+
 enum MenuAction { logout }
 
 class NotesView extends StatefulWidget {
@@ -25,7 +27,7 @@ class _NotesViewState extends State<NotesView> {
                 if (shouldLogout) {
                   await FirebaseAuth.instance.signOut();
                   Navigator.of(context)
-                      .pushNamedAndRemoveUntil("/login/", (route) => false);
+                      .pushNamedAndRemoveUntil(loginRoute, (route) => false);
                 }
             }
           }, itemBuilder: (context) {
